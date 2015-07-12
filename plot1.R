d66ae7d5b33ff855103dc09f33d54039fade687d
BhishmaRao <bhishma.gajavelli@gmail.com>
2015-07-12 17:30:50 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,15 @@
+HPC <- read.csv("~/household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
+dim(HPC)
+attach(HPC)
+subset <- Date=="1/2/2007" | Date=="2/2/2007"
+HPC2 <- HPC[subset, ]
+attach(HPC2)
+x <- paste(Date, Time)
+HPC2$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(HPC2) <- 1:nrow(HPC2)
+dim(HPC2) 
+attach(HPC2)
+
 source("HPCLoad.R")
 png(filename = "plot1.png", 
     width = 480, height = 480, 
